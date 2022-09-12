a312ca5d34711dd3bc3660854007ebfca5e4a9f7
mengtongun <unmengtong@gmail.com>
2022-09-12 17:31:29 +0700

    feat: product qr code discount

--- web/index.js
+++ web/index.js
@@ -12,6 +12,9 @@ import productCreator from "./helpers/product-creator.js";
 import redirectToAuth from "./helpers/redirect-to-auth.js";
 import { BillingInterval } from "./helpers/ensure-billing.js";
 import { AppInstallations } from "./app_installations.js";
+import applyQrCodeApiEndpoints from "./middleware/qr-code-api.js";
+import { QRCodesDB } from "./qr-codes-db.js";
+import applyQrCodePublicEndpoints from "./middleware/qr-code-public.js";
 
 const USE_ONLINE_TOKENS = false;
 
@@ -21,36 +24,38 @@ const PORT = parseInt(process.env.BACKEND_PORT || process.env.PORT, 10);
 const DEV_INDEX_PATH = `${process.cwd()}/frontend/`;
 const PROD_INDEX_PATH = `${process.cwd()}/frontend/dist/`;
 
-const DB_PATH = `${process.cwd()}/database.sqlite`;
-
+const dbFile = join(process.cwd(), "database.sqlite");
+const sessionDb = new Shopify.Session.SQLiteSessionStorage(dbFile);
+// Initialize SQLite DB
+QRCodesDB.db = sessionDb.db;
+QRCodesDB.init();
 Shopify.Context.initialize({
-  API_KEY: process.env.SHOPIFY_API_KEY,
-  API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
-  SCOPES: process.env.SCOPES.split(","),
-  HOST_NAME: process.env.HOST.replace(/https?:\/\//, ""),
-  HOST_SCHEME: process.env.HOST.split("://")[0],
-  API_VERSION: LATEST_API_VERSION,
-  IS_EMBEDDED_APP: true,
-  // This should be replaced with your preferred storage strategy
-  SESSION_STORAGE: new Shopify.Session.SQLiteSessionStorage(DB_PATH),
+	API_KEY: process.env.SHOPIFY_API_KEY,
+	API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
+	SCOPES: process.env.SCOPES.split(","),
+	HOST_NAME: process.env.HOST.replace(/https?:\/\//, ""),
+	HOST_SCHEME: process.env.HOST.split("://")[0],
+	API_VERSION: LATEST_API_VERSION,
+	IS_EMBEDDED_APP: true,
+	SESSION_STORAGE: sessionDb,
 });
 
 Shopify.Webhooks.Registry.addHandler("APP_UNINSTALLED", {
-  path: "/api/webhooks",
-  webhookHandler: async (_topic, shop, _body) => {
-    await AppInstallations.delete(shop);
-  },
+	path: "/api/webhooks",
+	webhookHandler: async (_topic, shop, _body) => {
+		await AppInstallations.delete(shop);
+	},
 });
 
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
 // See the ensureBilling helper to learn more about billing in this template.
 const BILLING_SETTINGS = {
-  required: false,
-  // This is an example configuration that would do a one-time charge for $5 (only USD is currently supported)
-  // chargeName: "My Shopify One-Time Charge",
-  // amount: 5.0,
-  // currencyCode: "USD",
-  // interval: BillingInterval.OneTime,
+	required: false,
+	// This is an example configuration that would do a one-time charge for $5 (only USD is currently supported)
+	// chargeName: "My Shopify One-Time Charge",
+	// amount: 5.0,
+	// currencyCode: "USD",
+	// interval: BillingInterval.OneTime,
 };
 
 // This sets up the mandatory GDPR webhooks. You’ll need to fill in the endpoint
@@ -63,138 +68,120 @@ setupGDPRWebHooks("/api/webhooks");
 
 // export for test use only
 export async function createServer(
-  root = process.cwd(),
-  isProd = process.env.NODE_ENV === "production",
-  billingSettings = BILLING_SETTINGS
+	root = process.cwd(),
+	isProd = process.env.NODE_ENV === "production",
+	billingSettings = BILLING_SETTINGS
 ) {
-  const app = express();
+	const app = express();
 
-  app.set("use-online-tokens", USE_ONLINE_TOKENS);
-  app.use(cookieParser(Shopify.Context.API_SECRET_KEY));
+	app.set("use-online-tokens", USE_ONLINE_TOKENS);
+	app.use(cookieParser(Shopify.Context.API_SECRET_KEY));
 
-  applyAuthMiddleware(app, {
-    billing: billingSettings,
-  });
+	applyAuthMiddleware(app, {
+		billing: billingSettings,
+	});
 
-  // Do not call app.use(express.json()) before processing webhooks with
-  // Shopify.Webhooks.Registry.process().
-  // See https://github.com/Shopify/shopify-api-node/blob/main/docs/usage/webhooks.md#note-regarding-use-of-body-parsers
-  // for more details.
-  app.post("/api/webhooks", async (req, res) => {
-    try {
-      await Shopify.Webhooks.Registry.process(req, res);
-      console.log(`Webhook processed, returned status code 200`);
-    } catch (e) {
-      console.log(`Failed to process webhook: ${e.message}`);
-      if (!res.headersSent) {
-        res.status(500).send(e.message);
-      }
-    }
-  });
+	// Do not call app.use(express.json()) before processing webhooks with
+	// Shopify.Webhooks.Registry.process().
+	// See https://github.com/Shopify/shopify-api-node/blob/main/docs/usage/webhooks.md#note-regarding-use-of-body-parsers
+	// for more details.
+	app.post("/api/webhooks", async (req, res) => {
+		try {
+			await Shopify.Webhooks.Registry.process(req, res);
+			console.log(`Webhook processed, returned status code 200`);
+		} catch (e) {
+			console.log(`Failed to process webhook: ${e.message}`);
+			if (!res.headersSent) {
+				res.status(500).send(e.message);
+			}
+		}
+	});
 
-  // All endpoints after this point will require an active session
-  app.use(
-    "/api/*",
-    verifyRequest(app, {
-      billing: billingSettings,
-    })
-  );
+	// All endpoints after this point will require an active session
+	app.use(
+		"/api/*",
+		verifyRequest(app, {
+			billing: billingSettings,
+		})
+	);
 
-  app.get("/api/products/count", async (req, res) => {
-    const session = await Shopify.Utils.loadCurrentSession(
-      req,
-      res,
-      app.get("use-online-tokens")
-    );
-    const { Product } = await import(
-      `@shopify/shopify-api/dist/rest-resources/${Shopify.Context.API_VERSION}/index.js`
-    );
+	app.get("/api/products/count", async (req, res) => {
+		const session = await Shopify.Utils.loadCurrentSession(
+			req,
+			res,
+			app.get("use-online-tokens")
+		);
+		const { Product } = await import(
+			`@shopify/shopify-api/dist/rest-resources/${Shopify.Context.API_VERSION}/index.js`
+		);
 
-    const countData = await Product.count({ session });
-    res.status(200).send(countData);
-  });
+		const countData = await Product.count({ session });
+		res.status(200).send(countData);
+	});
 
-  app.get("/api/products/create", async (req, res) => {
-    const session = await Shopify.Utils.loadCurrentSession(
-      req,
-      res,
-      app.get("use-online-tokens")
-    );
-    let status = 200;
-    let error = null;
+	// All endpoints after this point will have access to a request.body
+	// attribute, as a result of the express.json() middleware
+	app.use(express.json());
+	applyQrCodeApiEndpoints(app);
 
-    try {
-      await productCreator(session);
-    } catch (e) {
-      console.log(`Failed to process products/create: ${e.message}`);
-      status = 500;
-      error = e.message;
-    }
-    res.status(status).send({ success: status === 200, error });
-  });
+	app.use((req, res, next) => {
+		const shop = Shopify.Utils.sanitizeShop(req.query.shop);
+		if (Shopify.Context.IS_EMBEDDED_APP && shop) {
+			res.setHeader(
+				"Content-Security-Policy",
+				`frame-ancestors https://${encodeURIComponent(
+					shop
+				)} https://admin.shopify.com;`
+			);
+		} else {
+			res.setHeader("Content-Security-Policy", `frame-ancestors 'none';`);
+		}
+		next();
+	});
 
-  // All endpoints after this point will have access to a request.body
-  // attribute, as a result of the express.json() middleware
-  app.use(express.json());
+	if (isProd) {
+		const compression = await import("compression").then(
+			({ default: fn }) => fn
+		);
+		const serveStatic = await import("serve-static").then(
+			({ default: fn }) => fn
+		);
+		app.use(compression());
+		app.use(serveStatic(PROD_INDEX_PATH, { index: false }));
+	}
 
-  app.use((req, res, next) => {
-    const shop = Shopify.Utils.sanitizeShop(req.query.shop);
-    if (Shopify.Context.IS_EMBEDDED_APP && shop) {
-      res.setHeader(
-        "Content-Security-Policy",
-        `frame-ancestors https://${encodeURIComponent(
-          shop
-        )} https://admin.shopify.com;`
-      );
-    } else {
-      res.setHeader("Content-Security-Policy", `frame-ancestors 'none';`);
-    }
-    next();
-  });
+	app.use("/*", async (req, res, next) => {
+		if (typeof req.query.shop !== "string") {
+			res.status(500);
+			return res.send("No shop provided");
+		}
 
-  if (isProd) {
-    const compression = await import("compression").then(
-      ({ default: fn }) => fn
-    );
-    const serveStatic = await import("serve-static").then(
-      ({ default: fn }) => fn
-    );
-    app.use(compression());
-    app.use(serveStatic(PROD_INDEX_PATH, { index: false }));
-  }
+		const shop = Shopify.Utils.sanitizeShop(req.query.shop);
+		const appInstalled = await AppInstallations.includes(shop);
 
-  app.use("/*", async (req, res, next) => {
+		if (!appInstalled) {
+			return redirectToAuth(req, res, app);
+		}
 
-    if (typeof req.query.shop !== "string") {
-      res.status(500);
-      return res.send("No shop provided");
-    }
+		if (Shopify.Context.IS_EMBEDDED_APP && req.query.embedded !== "1") {
+			const embeddedUrl = Shopify.Utils.getEmbeddedAppUrl(req);
 
-    const shop = Shopify.Utils.sanitizeShop(req.query.shop);
-    const appInstalled = await AppInstallations.includes(shop);
+			return res.redirect(embeddedUrl + req.path);
+		}
 
-    if (!appInstalled) {
-      return redirectToAuth(req, res, app);
-    }
+		const htmlFile = join(
+			isProd ? PROD_INDEX_PATH : DEV_INDEX_PATH,
+			"index.html"
+		);
 
-    if (Shopify.Context.IS_EMBEDDED_APP && req.query.embedded !== "1") {
-      const embeddedUrl = Shopify.Utils.getEmbeddedAppUrl(req);
+		return res
+			.status(200)
+			.set("Content-Type", "text/html")
+			.send(readFileSync(htmlFile));
+	});
+	applyQrCodePublicEndpoints(app);
 
-      return res.redirect(embeddedUrl + req.path);
-    }
-
-    const htmlFile = join(
-      isProd ? PROD_INDEX_PATH : DEV_INDEX_PATH,
-      "index.html"
-    );
-
-    return res
-      .status(200)
-      .set("Content-Type", "text/html")
-      .send(readFileSync(htmlFile));
-  });
-
-  return { app };
+	return { app };
 }
 
 createServer().then(({ app }) => app.listen(PORT));
